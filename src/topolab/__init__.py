1bbc5bf65e77a68992b697d39fa7a40f577efd4a
keith1117 <w1715714000@gmail.com>
2026-09-17 20:19:42 -0400

    feat: add SIMP compliance sensitivity

--- src/topolab/__init__.py
+++ src/topolab/__init__.py
@@ -12,21 +12,25 @@ from topolab.fem import (
 )
 from topolab.mesh import Hex8Mesh, generate_structured_hex8
 from topolab.model import FaceLoad, FixedFaceSupport, PointLoad
+from topolab.simp import ComplianceResult, evaluate_compliance, simp_element_moduli
 
 __version__ = "0.1.0"
 
 __all__ = [
     "Hex8Mesh",
     "LinearStaticResult",
+    "ComplianceResult",
     "FaceLoad",
     "FixedFaceSupport",
     "PointLoad",
     "assemble_global_stiffness",
     "build_constrained_dofs",
     "build_load_vector",
+    "evaluate_compliance",
     "generate_structured_hex8",
     "hex8_element_stiffness",
     "isotropic_elasticity_matrix",
     "select_face_nodes",
+    "simp_element_moduli",
     "solve_linear_static",
 ]
